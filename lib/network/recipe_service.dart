df7c735d03e0fb4723662ec0f728eb25f26d1983
dchaitu <chaitanyadokara@gmail.com>
2021-12-18 11:24:58 +0530

    Added Intercepter

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,36 @@
+// 1
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
+
+// 2
+const String apiKey = '66bb41e4d7d1043d800c3e2e6313fbee';
+const String apiId = '<1b438e7a';
+// 3
+const String apiUrl = 'https://api.edamam.com';
+
+// 1
+@ChopperApi()
+// 2
+abstract class RecipeService extends ChopperService {
+  // 3
+  @Get(path: 'search')
+  // 4
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+      // 5
+      @Query('q') String query, @Query('from') int from, @Query('to') int to);
+// TODO: Add create()
+}
+Request _addQuery(Request req) {
+  // 1
+  final params = Map<String, dynamic>.from(req.parameters);
+  // 2
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+  // 3
+  return req.copyWith(parameters: params);
+}
+
+
+
